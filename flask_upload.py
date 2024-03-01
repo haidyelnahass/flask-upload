965d5ce1807299178d1fc7f351ad16d551447952
Haidy Elnahass <74070220+haidyelnahass@users.noreply.github.com>
2024-03-01 11:39:14 +0200

    add flask upload file

---
+++ flask_upload.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def uploadfile():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file:
+        file.save('uploads/' + file.filename)
+        return 'File uploaded successfully'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
